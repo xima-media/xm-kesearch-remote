5557978796d021a8e4597672fc1126a049e0f588
Maik Schneider <mas@xima.de>
2024-07-10 13:29:26 +0200

    feat: add test fixtures

---
+++ Tests/Fixtures/sys_template.sql
@@ -0,0 +1,2 @@
+insert into `sys_template` (`pid`, `title`, `root`, `clear`, `include_static_file`)
+values (1, 'MAIN TEMPLATE', 1, 3, 'EXT:bootstrap_package/Configuration/TypoScript,EXT:ke_search/Configuration/TypoScript');
--- Tests/Functional/Command/FetchContentCommandTest.php
+++ Tests/Functional/Command/FetchContentCommandTest.php
@@ -4,11 +4,13 @@ namespace Xima\XmKesearchRemote\Tests\Functional\Command;
 
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 use Xima\XmKesearchRemote\Command\FetchContentCommand;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class FetchContentCommandTest extends FunctionalTestCase
 {
-
     protected array $testExtensionsToLoad = [
+        'typo3conf/ext/ke_search',
         'typo3conf/ext/xm_kesearch_remote',
     ];
 
@@ -16,9 +18,22 @@ class FetchContentCommandTest extends FunctionalTestCase
         'typo3conf/ext/xm_kesearch_remote/Tests/Fixtures' => 'fileadmin/Fixtures',
     ];
 
-    public function testFixtures(): void
+    private CommandTester $commandTester;
+
+    public function testUrlCrawler(): void
     {
-        $this->assertFileExists('fileadmin/Fixtures/sitemap.xml');
+        $this->importCSVDataSet(__DIR__ . '/../../Fixtures/tx_kesearch_indexerconfig.csv');
+        $this->commandTester->execute([]);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $extensionConfigurationMock = $this->createMock(ExtensionConfiguration::class);
+
+        $command = new FetchContentCommand($extensionConfigurationMock);
+        $this->commandTester = new CommandTester($command);
     }
 
 }
